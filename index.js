42acda1d17e80cb26f104f765b518203c257a81b
gulatigaurav <gaurav@townsquare.co>
2020-08-19 17:40:23 +0530

    Update Start Script

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-require('./mongoose');
-const routes = require('./routes');
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+require("./mongoose");
+const routes = require("./routes");
 
 const app = express();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || "3000";
 
 /*app.use(express.static(path.join(__dirname, '../../node_modules/material-dashboard-react/dist')))
 app.use(/^((?!(api)).)*!/, (req, res) => {
@@ -14,10 +14,10 @@ app.use(/^((?!(api)).)*!/, (req, res) => {
 
 app.use(cors());
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
 
 module.exports = app;
